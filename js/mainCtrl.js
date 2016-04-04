32fb2e0b1770c4533ab224108fdd4f866476a0a1
BorcheIvanov <borche.ivanov@outlook.com>
2016-04-04 16:10:00 +0200

    Signed-off-by: BorcheIvanov <borche.ivanov@outlook.com>

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,4 +1,19 @@
-var app = angular.module('myApp', []);
-app.controller('ctrl', function($scope){
+var app = angular.module('myApp', ['ngRoute']);
+
+app.config(function($routeProvider){
+	$routeProvider
+	.when('/',{
+		templateUrl: 'login.html',
+		controller: 'logCtrl'
+	})
+	.when('/register',{
+		templateUrl: 'register.html'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+});
+
+app.controller('logCtrl', function($scope, $location){
 	
 });
