3ec7fde61c53256c74efcdabda15257ffcfc5d2e
Milos Nikolik <milospolarcape@gmail.com>
2016-04-14 16:54:04 +0200

    change is good

--- events/src/main/java/mk/polarcape/service/impl/Employee_eventServiceImpl.java
+++ events/src/main/java/mk/polarcape/service/impl/Employee_eventServiceImpl.java
@@ -1,5 +1,6 @@
 package mk.polarcape.service.impl;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +11,10 @@ import org.springframework.stereotype.Service;
 import mk.polarcape.model.Employee;
 import mk.polarcape.model.Employee_event;
 import mk.polarcape.model.Event;
-import mk.polarcape.repository.Employee_eventRepository;
-import mk.polarcape.service.Employee_eventService;
 import mk.polarcape.repository.EmployeeRepository;
+import mk.polarcape.repository.Employee_eventRepository;
 import mk.polarcape.repository.EventRepository;
+import mk.polarcape.service.Employee_eventService;
 
 @Service
 public class Employee_eventServiceImpl implements Employee_eventService {
@@ -36,17 +37,18 @@ public class Employee_eventServiceImpl implements Employee_eventService {
 	}
 
 	public Employee_event save(Employee_event Employee_event) {
+		//get needed event and employees
 	         Event evn = EventRepository.findById(Employee_event.getEvents_id().getId());
 	         Employee emp = EmployeeRepository.findById(Employee_event.getInvited_id().getId());
 	         Employee emph = EmployeeRepository.findById(Employee_event.getHosting_id().getId());
 	        SimpleMailMessage mailMessage=new SimpleMailMessage();
 	          try{     
 	       mailMessage.setTo(emp.getEmail());
-	       mailMessage.setFrom("milospolarcape@outlook.com");
+	       mailMessage.setFrom("polarcape@outlook.com");
 	        mailMessage.setSubject("Event");
 	        mailMessage.setText("Dear " +emp.getName() +"\n "+emp.getEmail() + "\n You have been invited to attend the event: "
-	        		+ evn.getName() + " created by:" + emph.getName() + "\n"
-	        				+ "money for the event is "+ Employee_event.getMoneyOWNED() );
+	        		+ evn.getName() + ". Event created by:" + emph.getName() + ".\n"
+	        				+ "Money for the event is "+ Employee_event.getMoneyOWNED() + "\n Have a nice time !!!" );
 	        
 	        System.out.println("Dear " +emp.getName() +"\n "+emp.getEmail() +"\n You have been invited to attend the event: "
 	        		+ evn.getName() + " created by: " + emp.getName() + "\n"
@@ -75,5 +77,37 @@ public class Employee_eventServiceImpl implements Employee_eventService {
 		return Employee_eventRepository.selectWhere(invited_id);
 	
 	}
-
+	
+	///send mail to people who didnt pay 1 day prior expiration date
+	public void mailNotifier(){
+		Date cd =new Date();//datum current
+		List<Event> evn = EventRepository.findAll();
+		for(Event ev:evn){
+			Date dayBefore = new Date(ev.getExpirationDate().getTime() - 24*3600*1000);
+			if(cd.compareTo(dayBefore)>=0){
+			List<Employee_event> empev = Employee_eventRepository.selectInvited(ev.getId());
+			for(Employee_event ee:empev){
+				if(ee.getMoneyOWNED()>0){
+       SimpleMailMessage mailMessage=new SimpleMailMessage();
+         try{     
+      mailMessage.setTo(ee.getInvited_id().getEmail());
+      mailMessage.setFrom("polarcape@outlook.com");
+       mailMessage.setSubject("Event");
+       mailMessage.setText("Dear " +ee.getInvited_id().getName() +"\n "+ee.getInvited_id().getEmail() + "\n You have to pay for the event: "
+       		+ ev.getName() + ". Event created by:" + ev.getCreatedBy() + ".\n"
+       				+ " Expiration date is in 1 day. Money to pay " + ee.getMoneyOWNED() );
+       javaMailService.send(mailMessage);
+       System.out.println("Dear " +ee.getInvited_id().getName() +"\n "+ee.getInvited_id().getEmail() + "\n You have to pay for the event: "
+          		+ ev.getName() + ". Event created by:" + ev.getCreatedBy() + ".\n"
+   				+ " Expiration date is in 1 day. Money to pay " + ee.getMoneyOWNED() );
+         }
+         catch(Exception e){
+       	  System.out.println("greska");
+       	  System.out.println(e);
+         }
+				}//if money
+				}//for ee
+				}// if exp date
+				}// for ev
+	}
 }
