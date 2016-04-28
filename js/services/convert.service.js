7da1f9044538326810c3749cc9310dcee52c1c65
BorcheIvanov <borche.ivanov@outlook.com>
2016-04-28 15:17:56 +0200

    home page fixed

---
+++ js/services/convert.service.js
@@ -0,0 +1,12 @@
+angular.module('myApp')
+
+	.factory('ceil', function(){
+		return{
+			money: function(niza){
+				for(i = 0; i < niza.length; i++){
+					niza[i].moneyOWNED = Math.ceil(niza[i].moneyOWNED);
+				}
+				return niza;
+			}
+		}
+	});
